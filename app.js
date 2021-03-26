a7a31b88d72a43f9961e54998dbdc09055da707b
Kayla Daugherty <kayla.daugherty@gmail.com>
2021-03-26 10:21:45 -0700

    updated calc stretch

--- app.js
+++ app.js
@@ -1,58 +1,60 @@
 import { add, multiplication, subtract, divide, toNumber } from './calculator.js';
 
+calc('addition-input1', 'addition-input2', 'addition-button', 'addition-result', add);
+// const addition1Input = document.getElementById('addition-input1');
+// const addition2Input = document.getElementById('addition-input2');
+// const additionButton = document.getElementById('addition-button');
+// const additionResult = document.getElementById('addition-result');
 
-const addition1Input = document.getElementById('addition-input1');
-const addition2Input = document.getElementById('addition-input2');
-const additionButton = document.getElementById('addition-button');
-const additionResult = document.getElementById('addition-result');
+// additionButton.addEventListener('click', () =>{
 
-additionButton.addEventListener('click', () =>{
+//     const x = toNumber(addition1Input);
+//     const y = toNumber(addition2Input);    
+//     const sum = add(x, y);
+//     additionResult.textContent = sum;
 
-    const x = toNumber(addition1Input);
-    const y = toNumber(addition2Input);    
-    const sum = add(x, y);
-    additionResult.textContent = sum;
+// });
 
-});
+calc('subtraction-input1', 'subtraction-input2', 'subtraction-button', 'subtraction-result', subtract)
+// const subtraction1Input = document.getElementById('subtraction-input1');
+// const subtraction2Input = document.getElementById('subtraction-input2');
+// const subtractionButton = document.getElementById('subtraction-button');
+// const subtractionResult = document.getElementById('subtraction-result');
 
-const subtraction1Input = document.getElementById('subtraction-input1');
-const subtraction2Input = document.getElementById('subtraction-input2');
-const subtractionButton = document.getElementById('subtraction-button');
-const subtractionResult = document.getElementById('subtraction-result');
-
-subtractionButton.addEventListener('click', () =>{
-    const x = toNumber(subtraction1Input);
-    const y = toNumber(subtraction2Input);    
-    const sum = subtract(x, y);
-    subtractionResult.textContent = sum;
+// subtractionButton.addEventListener('click', () =>{
+//     const x = toNumber(subtraction1Input);
+//     const y = toNumber(subtraction2Input);    
+//     const sum = subtract(x, y);
+//     subtractionResult.textContent = sum;
    
-});
+// });
 
-const multiplication1Input = document.getElementById('multiplication-input1');
-const multiplication2Input = document.getElementById('multiplication-input2');
-const multiplicationButton = document.getElementById('multiplication-button');
-const multiplicationResult = document.getElementById('multiplication-result');
+calc('multiplication-input1', 'multiplication-input2', 'multiplication-button', 'multiplication-result', multiplication)
+// const multiplication1Input = document.getElementById('multiplication-input1');
+// const multiplication2Input = document.getElementById('multiplication-input2');
+// const multiplicationButton = document.getElementById('multiplication-button');
+// const multiplicationResult = document.getElementById('multiplication-result');
 
-multiplicationButton.addEventListener('click', () =>{
-    const x = toNumber(multiplication1Input);
-    const y = toNumber(multiplication2Input);    
-    const sum = multiplication(x, y);
-    multiplicationResult.textContent = sum;
+// multiplicationButton.addEventListener('click', () =>{
+//     const x = toNumber(multiplication1Input);
+//     const y = toNumber(multiplication2Input);    
+//     const sum = multiplication(x, y);
+//     multiplicationResult.textContent = sum;
   
-});
+// });
 
-const division1Input = document.getElementById('division-input1');
-const division2Input = document.getElementById('division-input2');
-const divisionButton = document.getElementById('division-button');
-const divisionResult = document.getElementById('division-result');
+calc('division-input1', 'division-input2', 'division-button', 'division-result', divide)
+// const division1Input = document.getElementById('division-input1');
+// const division2Input = document.getElementById('division-input2');
+// const divisionButton = document.getElementById('division-button');
+// const divisionResult = document.getElementById('division-result');
 
-divisionButton.addEventListener('click', () =>{
-    const x = toNumber(division1Input);
-    const y = toNumber(division2Input);    
-    const sum = divide(x, y);
-    divisionResult.textContent = sum;
-});
-
+// divisionButton.addEventListener('click', () =>{
+//     const x = toNumber(division1Input);
+//     const y = toNumber(division2Input);    
+//     const sum = divide(x, y);
+//     divisionResult.textContent = sum;
+// });
 
 function calc(input1Id, input2Id, buttonId, displayId, operation) {
     const input1 = document.getElementById(input1Id);
@@ -60,12 +62,13 @@ function calc(input1Id, input2Id, buttonId, displayId, operation) {
     const button = document.getElementById(buttonId);
     const resultDisplay = document.getElementById(displayId);
 
-button.addEventListener('click', () =>{
-    const x = toNumber(input1);
-    const y = toNumber(input2);    
-    const result = operation(x, y);
-    divisionResult.textContent = result;
-    input1.value = '';
-    input2.value = '';
-}
 
+    button.addEventListener('click', () =>{
+        const x = toNumber(input1);
+        const y = toNumber(input2);    
+        const result = operation(x, y);
+        resultDisplay.textContent = result;
+        input1.value = '';
+        input2.value = '';
+    });
+}
